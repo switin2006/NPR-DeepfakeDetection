e40134ad76a700752fc968e77d10d1d152a502ac
switin2006 <switinmechineni@gmail.com>
2025-10-02 17:42:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,73 +1,43 @@
-import sys
-import time
 import os
-import csv
 import torch
-from util import Logger, printSet
-from validate import validate
-from networks.resnet import resnet50
-from options.test_options import TestOptions
-import networks.resnet as resnet
 import numpy as np
+from validate import validate
+from networks.trainer import Trainer
+from options.test_options import TestOptions
 import random
-import random
+
 def seed_torch(seed=1029):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.enabled = False
-seed_torch(100)
-DetectionTests = {
-                'ForenSynths': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/ForenSynths/',
-                                 'no_resize'  : False, # Due to the different shapes of images in the dataset, resizing is required during batch detection.
-                                 'no_crop'    : True,
-                               },
 
-           'GANGen-Detection': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/GANGen-Detection/',
-                                 'no_resize'  : True,
-                                 'no_crop'    : True,
-                               },
+if __name__ == '__main__':
+    seed_torch(100)
+    opt = TestOptions().parse()
 
-         'DiffusionForensics': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/DiffusionForensics/',
-                                 'no_resize'  : False, # Due to the different shapes of images in the dataset, resizing is required during batch detection.
-                                 'no_crop'    : True,
-                               },
+    opt.isTrain = False 
+    model = Trainer(opt)
+    model.load_networks(opt.resume)
+    
+    print(f"--- Evaluating model: {opt.resume} ---")
+    print(f"--- On test dataset: {opt.dataroot} ---")
 
-        'UniversalFakeDetect': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/UniversalFakeDetect/',
-                                 'no_resize'  : False, # Due to the different shapes of images in the dataset, resizing is required during batch detection.
-                                 'no_crop'    : True,
-                               },
+    model.eval()
+    
+    # Unpack the new return values from the validate function
+    acc, ap, auc, _, _, precision, f1_score = validate(model.model, opt)
 
-                 }
-
-
-opt = TestOptions().parse(print_options=False)
-print(f'Model_path {opt.model_path}')
-
-# get model
-model = resnet50(num_classes=1)
-model.load_state_dict(torch.load(opt.model_path, map_location='cpu'), strict=True)
-model.cuda()
-model.eval()
-
-for testSet in DetectionTests.keys():
-    dataroot = DetectionTests[testSet]['dataroot']
-    printSet(testSet)
-
-    accs = [];aps = []
-    print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
-    for v_id, val in enumerate(os.listdir(dataroot)):
-        opt.dataroot = '{}/{}'.format(dataroot, val)
-        opt.classes  = '' #os.listdir(opt.dataroot) if multiclass[v_id] else ['']
-        opt.no_resize = DetectionTests[testSet]['no_resize']
-        opt.no_crop   = DetectionTests[testSet]['no_crop']
-        acc, ap, _, _, _, _ = validate(model, opt)
-        accs.append(acc);aps.append(ap)
-        print("({} {:12}) acc: {:.1f}; ap: {:.1f}".format(v_id, val, acc*100, ap*100))
-    print("({} {:10}) acc: {:.1f}; ap: {:.1f}".format(v_id+1,'Mean', np.array(accs).mean()*100, np.array(aps).mean()*100));print('*'*25) 
+    print('*' * 35)
+    print(f"Evaluation Results for CIFAKE Test Set:")
+    print(f"  Accuracy:           {acc*100:.2f}%")
+    print(f"  Average Precision:  {ap*100:.2f}%")
+    print(f"  Precision:          {precision*100:.2f}%")
+    print(f"  F1-Score:           {f1_score*100:.2f}%")
+    print('*' * 35)
 
